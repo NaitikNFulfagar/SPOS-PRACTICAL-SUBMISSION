8e0d099fe8c88baea1225d93cd3f7f112596c2f3
Naitik Fulfagar <naitiknfulfagar@gmail.com>
2023-11-17 07:30:52 +0530

    Update 4.Priority.java

--- 4.Priority.java
+++ 4.Priority.java
@@ -1,63 +1,69 @@
 import java.util.*;
-public class priority
+
+public class Main 
 {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        int x,n,p[],pp[],wt[],ta[],bt[],awt=0,ata = 0;
-        p=new int[10];
-        pp=new int[10];
-        bt=new int[10];
-        wt=new int[10];
-        ta=new int[10];
-        System.out.println("ENter the number of proceses: ");
-        n=sc.nextInt();
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of processes: ");
+        int n=sc.nextInt();
+        
+        int bt[]=new int[n];
+        int pri[]=new int[n];
+        int tat[]=new int[n];
+        int wt[]=new int[n];
+        int pID[]=new int[n];
+        
+        int temp;
+        float avg_tat=0,avg_wt=0;
+        
         for(int i=0;i<n;i++)
         {
-            System.out.println("Enter burst time of process: "+(i+1));
+            System.out.println("Enter the burst time of process "+(i+1)+" :");
             bt[i]=sc.nextInt();
-            System.out.println("Enter the priority: ");
-            pp[i]=sc.nextInt();
-            p[i]=i+1;
+            System.out.println("Enter the time priority of process "+(i+1)+" :");
+            pri[i]=sc.nextInt();
+            pID[i]=i+1;
         }
+        
         for(int i=0;i<n-1;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(pp[i]>pp[j])
+                if(pri[i]>pri[j])
                 {
-                    x=pp[i];
-                    pp[i]=pp[j];
-                    pp[j]=x;
-
-                    x=bt[i];
+                    temp=pri[i];
+                    pri[i]=pri[j];
+                    pri[j]=temp;
+                    
+                    temp=pID[i];
+                    pID[i]=pID[j];
+                    pID[j]=temp;
+                    
+                    temp=bt[i];
                     bt[i]=bt[j];
-                    bt[j]=x;
-
-                    x=p[i];
-                    p[i]=p[j];
-                    p[j]=x;
+                    bt[j]=temp;
                 }
             }
         }
         wt[0]=0;
-        ta[0]=bt[0];
-        ata+=ta[0];
+        avg_wt=0;
+        tat[0]=bt[0];
+        avg_tat=tat[0];
+        for(int i=1;i<n;i++)
+        {
+            wt[i]=tat[i-1];
+            avg_wt+=wt[i];
+            tat[i]=bt[i]+wt[i];
+            avg_tat+=tat[i];
+        }
+        
+        System.out.println("\nPID\tBurst\tPriority\tTAT\tWT\n");
         for(int i=0;i<n;i++)
         {
-            wt[i]=ta[i-1];
-            awt+=wt[i];
-            ta[i]=wt[i]+bt[i];
-            ata+=ta[i];
+            System.out.println("\n"+(i+1)+"\t"+bt[i]+"\t"+pri[i]+"\t"+tat[i]+"\t"+wt[i]+"\n");
         }
-        System.out.println("\n Process  Priority  Burst time  Wait time  Turnaround time  ");
-        for(int i=0;i<n;i++)
-        {
-            System.out.println(" "+p[i]+"  \t  "+pp[i]+"  \t  "+bt[i]+"  \t  "+wt[i]+"  \t  "+ta[i]);
-        }
-        awt/=n;
-        ata/=n;
-        System.out.println("Average wait time is: "+awt);
-        System.out.println("Average turnaround time is: "+ata);
+        System.out.println("Average waiting time: "+(avg_wt/n));
+        System.out.println("Average turn around time: "+(avg_tat/n));
     }
 }
+
